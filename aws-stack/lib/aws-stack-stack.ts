9a20552139e6507d7835efcecb80d64ba57aeb32
Dickinson Arismendy <thordickinson@gmail.com>
2025-03-10 17:33:30 -0500

    feat: primera versión de leaflet

--- aws-stack/lib/aws-stack-stack.ts
+++ aws-stack/lib/aws-stack-stack.ts
@@ -1,16 +1,15 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
 export class AwsStackStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsStackQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const helloWorldLambda = new lambda.Function(this, 'HelloWorldLambda', {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: 'hello-world-lambda.handler',
+      code: lambda.Code.fromAsset('lib'),
+    });
   }
 }
