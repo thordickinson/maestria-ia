6e4e4fbb41f8463f0ce25b51b609229f416a3430
Dickinson Arismendy <thordickinson@gmail.com>
2025-03-11 10:59:54 -0500

    feat: map layer

--- proyecto-nextjs/src/components/checkbox-button.tsx
+++ proyecto-nextjs/src/components/checkbox-button.tsx
@@ -1,7 +1,14 @@
 import { Checkbox } from "./ui/checkbox";
 
-export default function CheckboxButton(){
-    return <div>
-        <Checkbox></Checkbox>
+interface CheckboxButtonParams {
+    label: string
+    checked: boolean
+    onValueChanged: (value: boolean) => void
+}
+
+export default function CheckboxButton({label, checked, onValueChanged}: CheckboxButtonParams){
+    return <div className="px-1 py-2 bg-blue flex">
+        <Checkbox checked={checked} onChange={console.log}></Checkbox>
+        <span>{label}</span>
     </div>
 }
--- proyecto-nextjs/src/components/map.tsx
+++ proyecto-nextjs/src/components/map.tsx
@@ -1,48 +1,60 @@
-import { MapContainer, Marker, TileLayer, Popup } from "react-leaflet"
-import "leaflet/dist/leaflet.css"
-import "leaflet-defaulticon-compatibility"
-import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css"
-import { useEffect, useMemo, useRef } from "react"
-import { LatLng } from "@/lib/types"
-import { Button } from "./ui/button"
+"use client";
 
+import { MapContainer, Marker, TileLayer, Popup, LayersControl, LayerGroup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import "leaflet-defaulticon-compatibility";
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
+import { useEffect, useMemo, useRef } from "react";
+import { LatLng, MapLayer } from "@/lib/types";
+import { Button } from "./ui/button";
+import CheckboxButton from "./checkbox-button";
 
 interface MapProps {
-  position: LatLng
-  zoom: number
-  onMarkerLocationChanged?: (position: LatLng) => void
+  position: LatLng;
+  zoom: number;
+  onMarkerLocationChanged?: (position: LatLng) => void;
+  layers?: MapLayer[];
 }
 
-
-export default function Map({position, zoom, onMarkerLocationChanged}: MapProps) {
-  const markerRef = useRef<any>(null)
+export default function Map({ position, zoom, layers = [], onMarkerLocationChanged }: MapProps) {
+  const markerRef = useRef<any>(null);
 
   const eventHandlers = useMemo(
     () => ({
       dragend() {
-        const marker = markerRef.current
+        const marker = markerRef.current;
         if (marker != null && onMarkerLocationChanged) {
           onMarkerLocationChanged(marker.getLatLng());
         }
       },
     }),
-    [],
-  )
+    []
+  );
 
-  return <div>
-  <MapContainer center={position} zoom={zoom} scrollWheelZoom={false} style={{ height: "400px", width: "100%" }} attributionControl={false} >
-    <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    <Marker ref={markerRef} position={position} draggable={true} eventHandlers={eventHandlers}>
-      <Popup>
-        Mueve el marcador para seleccionar la posición de tu inmueble
-      </Popup>
-    </Marker>
-  </MapContainer>
-  <div>
-    <Button>Hola</Button>
-  </div>
-  </div>
+  return (
+    <div>
+      <MapContainer center={position} zoom={zoom} scrollWheelZoom={false} style={{ height: "400px", width: "100%" }} attributionControl={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <LayersControl position="bottomright">
+          {layers.map((layer) => (
+            <LayersControl.Overlay key={layer.key} name={layer.label} checked>
+              <LayerGroup>
+              {layer.markers.map((marker) => (
+                <Marker key={marker.key} position={marker.location}>
+                  <Popup>{marker.label}</Popup>
+                </Marker>
+              ))}
+              </LayerGroup>
+            </LayersControl.Overlay>
+          ))}
+        </LayersControl>
+        <Marker ref={markerRef} position={position} draggable={true} eventHandlers={eventHandlers}>
+          <Popup>Mueve el marcador para seleccionar la posición de tu inmueble</Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
 }
