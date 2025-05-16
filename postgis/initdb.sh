e695464520ea0cefc78f342f32dbc9803ddebfdc
Dickinson Arismendy <thordickinson@gmail.com>
2025-05-16 12:16:42 -0500

    feat: se remueve exceso de logs en la importación de datos

--- postgis/initdb.sh
+++ postgis/initdb.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 set -e
 
+# Función para imprimir con timestamp
+log() {
+  echo "$(date '+%Y-%m-%d %H:%M:%S') $*"
+}
+
 # Configuración de PostgreSQL
 DB_NAME="gisdb"
 DB_USER="postgres"
 DB_PASS="postgres"
 DATA_DIR="/shapefiles"
 
-echo "🟢 Verificando si la base de datos ya existe..."
+log "🟢 Verificando si la base de datos ya existe..."
 
 # Verifica si la base de datos ya fue inicializada
 if psql -U "$DB_USER" -lqt | cut -d \| -f 1 | grep -qw "$DB_NAME"; then
-  echo "✅ La base de datos $DB_NAME ya existe. Verificando tablas..."
+  log "✅ La base de datos $DB_NAME ya existe. Verificando tablas..."
 else
-  echo "🚀 Creando la base de datos y habilitando PostGIS..."
+  log "🚀 Creando la base de datos y habilitando PostGIS..."
   psql -U "$DB_USER" -c "CREATE DATABASE $DB_NAME;"
   psql -U "$DB_USER" -d "$DB_NAME" -c "CREATE EXTENSION postgis;"
 fi
 
-echo "📂 Buscando archivos SHP en $DATA_DIR..."
+log "📂 Buscando archivos SHP en $DATA_DIR..."
 
 # Procesar cada carpeta individualmente
 find "$DATA_DIR" -type d | while read DIR; do
@@ -39,14 +44,16 @@ find "$DATA_DIR" -type d | while read DIR; do
     TABLE_EXISTS=$(psql -U "$DB_USER" -d "$DB_NAME" -tAc "SELECT to_regclass('public.$TABLE_NAME') IS NOT NULL")
     
     if [ "$TABLE_EXISTS" == "t" ]; then
-      echo "✅ La tabla $TABLE_NAME ya existe. Saltando importación."
+      log "✅ La tabla $TABLE_NAME ya existe. Saltando importación."
     else
-      echo "🔄 Importando: $TABLE_NAME desde $(dirname "$SHP_FILE")"
-      shp2pgsql -I -s 4326 "$SHP_FILE" public."$TABLE_NAME" | psql -U "$DB_USER" -d "$DB_NAME"
-      echo "✅ Importado: $TABLE_NAME"
+      log "🔄 Importando: $TABLE_NAME desde $(dirname "$SHP_FILE")"
+      shp2pgsql -I -s 4326 "$SHP_FILE" public."$TABLE_NAME" | \
+      psql -q -U "$DB_USER" -d "$DB_NAME" | \
+      grep -v '^INSERT 0 1$'
+      log "✅ Importado: $TABLE_NAME"
     fi
   done
 
 done
 
-echo "🚀 Proceso de importación completado."
+log "🚀 Proceso de importación completado."
