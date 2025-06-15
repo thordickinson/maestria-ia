aafb05dee89e74a4d4d5e75938a70344bb80bda4
Dickinson Arismendy <thordickinson@gmail.com>
2025-06-14 19:08:19 -0500

    se elimina proyecto next

--- proyecto-nextjs/src/app/atribuciones/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function AttributionsPage(){
-    return <div>
-        <div>
-            Iconos de mapa provenientes de: https://www.flaticon.com/packs/location-pins-22
-        </div>
-    </div>
-}
--- proyecto-nextjs/src/components/ui/button-panel.tsx
+++
@@ -1,7 +0,0 @@
-import { PropsWithChildren } from "react";
-
-export default function ButtonPanel({children}: PropsWithChildren){
-    return <div className="flex flex-row gap-2">
-        {children}
-    </div>
-}
