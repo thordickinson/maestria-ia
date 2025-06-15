da64e592185702eeabccb16afc7420503a875879
Dickinson Arismendy <thordickinson@gmail.com>
2025-06-14 19:07:04 -0500

    feat: implementacion en react sin next

---
+++ estimador-react/src/components/form-field.tsx
@@ -0,0 +1,19 @@
+import type { PropsWithChildren } from "react";
+
+interface FormFieldProps {
+    label: string
+    editorId?: string
+    hint?: string
+}
+
+export default function FormField({children, label, editorId, hint}: PropsWithChildren<FormFieldProps>){
+    return <div className="flex flex-col">
+        <label htmlFor={editorId}>{label}</label>
+        <div className="pt-2">
+            {children}
+        </div>
+        <div className="text-xs h-[1em] mb-1">
+            {hint}
+        </div>
+    </div>
+}
---
+++ estimador-react/src/pages/atribuciones.tsx
@@ -0,0 +1,7 @@
+export default function AttributionsPage(){
+    return <div>
+        <div>
+            Iconos de mapa provenientes de: https://www.flaticon.com/packs/location-pins-22
+        </div>
+    </div>
+}
