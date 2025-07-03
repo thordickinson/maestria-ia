5d335dfaec53f143bae6d152d17604be047ad273
Dickinson Arismendy <thordickinson@gmail.com>
2025-07-02 22:08:17 -0500

    feat: estadisticas por regiones

--- indexador-py/server.py
+++ indexador-py/server.py
@@ -3,7 +3,7 @@ from typing import Annotated
 import uvicorn
 from pydantic import BaseModel
 
-from src.etl.geohash_stats import get_point_stats
+from src.etl.geohash_stats import get_point_stats, get_region_stats
 
 class EstimationRequest(BaseModel):
     lat: float
@@ -23,12 +23,14 @@ app = FastAPI()
 async def estimate(params: Annotated[EstimationRequest, Query()]):
     print(f"Received estimation request: {params.model_dump()}")
     stats = await get_point_stats(params.lat, params.lng)
+    region_stats = await get_region_stats(params.lat, params.lng)
     result = stats.model_dump()
     result["estimation"] = {
         "minValue": 295_000_000,
         "average": 300_000_000,
         "maxValue": 310_000_000
     }
+    result["region_stats"] = region_stats
     return result
 
 if __name__ == "__main__":
