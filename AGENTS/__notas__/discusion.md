c03161e42db779c0826727f19581bddeed3f329c
Dickinson Arismendy <thordickinson@gmail.com>
2025-10-23 12:30:00 -0500

    feat: progreso en resultados

---
+++ AGENTS/__notas__/discusion.md
@@ -0,0 +1,3 @@
+* Los precios de las propiedades, según la costumbre inmobiliaria, se realiza teniendo en cuenta el valor promedio del metro cuadrado de las propiedades cercanas al inmueble a vender.
+
+* 
--- documento/compile.sh
+++ documento/compile.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 set -e
 
-# Compile LaTeX document using a Docker LaTeX image
-docker run --rm -v "$(pwd)":/workdir -w /workdir texlive/texlive pdflatex main.tex
+# Compile LaTeX document (BibLaTeX + Biber) using a Docker TeX Live image
+IMG=texlive/texlive
+
+# First LaTeX pass
+docker run --rm -v "$(pwd)":/workdir -w /workdir "$IMG" pdflatex -interaction=nonstopmode -halt-on-error main.tex
+
+# Biber pass (bibliography)
+docker run --rm -v "$(pwd)":/workdir -w /workdir "$IMG" biber main
+
+# Second and third LaTeX passes to resolve references
+docker run --rm -v "$(pwd)":/workdir -w /workdir "$IMG" pdflatex -interaction=nonstopmode -halt-on-error main.tex
+docker run --rm -v "$(pwd)":/workdir -w /workdir "$IMG" pdflatex -interaction=nonstopmode -halt-on-error main.tex
